b725a5e807ee2c1b5fd861c007551d9d3eea47f4
naico <naicoLavoro>
2014-05-09 17:46:57 +0200

    organizzazione assets e landing
    organizzo gli assets in cartelle, uppo la trivella, programmo il landing

---
+++ Assets/Scripts/PlanetControls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetControls : MonoBehaviour {
+	
+	public float rotationSpeed = 600f;
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		// sarebbe più pulito catturare gli input come qui sotto
+		// ovvero usando le etichette dell'input manager del progetto..
+		//bool salto = Input.GetButton("Jump");
+		
+		// però lo faccio così..
+		if (Input.GetKey(KeyCode.A)) {
+			rigidbody2D.AddTorque(rotationSpeed * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.D)) {
+			rigidbody2D.AddTorque(-1 * rotationSpeed * Time.deltaTime);
+		}
+		
+		
+		if (Input.GetKey(KeyCode.Escape)) {
+			Application.Quit();
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	public float speedLanding = 100f;
+	bool landed = false;
+
+	void Start() {
+		landed = false;
+	}
+
+	void Update (){
+		rigidbody2D.velocity = new Vector2(0,-speedLanding) * Time.deltaTime;
+	}
+
+	void OnBecameInvisible() {
+		// funzionante
+		//print ("invisibile");
+		//transform.position = new Vector2(0, 4f );
+	}
+	
+	void OnBecameVisible() {
+		// funzionante
+		//print ("visibile");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		print ("toccato");
+		speedLanding = 0;
+		rigidbody2D.velocity = Vector2.zero;
+		GameObject obj=GameObject.Find("pianeta");
+		// attach a to b
+		transform.parent=obj.transform;
+	}
+
+}
