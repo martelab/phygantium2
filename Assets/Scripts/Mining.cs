fca911df843f9e1ced1b921127ff2bd679a2a6a2
martelab <web@martelab.org>
2014-05-10 12:12:19 +0200

    trivella da iena

---
+++ Assets/Scripts/Mining.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mining : MonoBehaviour {
+
+	public float miningSpeed = 0f;
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rigidbody2D.velocity = new Vector2(0,-miningSpeed) * Time.deltaTime;
+	}
+
+
+	void Mina(float speed) {
+		print ("mina!");
+	}
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,32 +6,42 @@ public class Ship : MonoBehaviour {
 	public float speedLanding = 100f;
 	bool landed = false;
 
+
 	void Start() {
 		landed = false;
 	}
 
+
 	void Update (){
 		rigidbody2D.velocity = new Vector2(0,-speedLanding) * Time.deltaTime;
 	}
+
 
 	void OnBecameInvisible() {
 		// funzionante
 		//print ("invisibile");
 		//transform.position = new Vector2(0, 4f );
 	}
-	
+
+
 	void OnBecameVisible() {
 		// funzionante
 		//print ("visibile");
 	}
 
+
 	void OnTriggerEnter2D(Collider2D other) {
+		// stop landing start mining!
 		print ("toccato");
 		speedLanding = 0;
 		rigidbody2D.velocity = Vector2.zero;
 		GameObject obj=GameObject.Find("pianeta");
 		// attach a to b
 		transform.parent=obj.transform;
+		GameObject trivella=GameObject.Find("trivella");
+		trivella.transform.position = new Vector3(transform.position.x, (transform.position.y-1f),0);
+		trivella.renderer.enabled = true;
+		trivella.SendMessage ("Mina", 5f);
 	}
 
 }
